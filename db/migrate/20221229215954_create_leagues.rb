3aa0ed8fd6dabe93565d550a7a395397dbb41e4e
notBpro <bmproell@gmail.com>
2022-12-29 17:07:28 -0500

    created leagues

---
+++ db/migrate/20221229215954_create_leagues.rb
@@ -0,0 +1,11 @@
+class CreateLeagues < ActiveRecord::Migration[7.0]
+  def change
+    create_table :leagues do |t|
+      t.string :name
+      t.string :description
+      t.string :code
+
+      t.timestamps
+    end
+  end
+end
